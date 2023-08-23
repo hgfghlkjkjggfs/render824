5e1635fe4af9bf7519e8610a05b7e96a150fcebd
hgfghlkjkjggfs <136891567+hgfghlkjkjggfs@users.noreply.github.com>
2023-08-23 15:38:16 -0800

    Update export.sh

--- main/config/export.sh
+++ main/config/export.sh
@@ -3,7 +3,7 @@
 export PORT=${PORT:-80}
 export USERNAME=${USERNAME:-admin}
 export PASSWORD=${PASSWORD:-admin}
-export UUID=${UUID:-1eb6e917-774b-4a84-aff6-b058577c60a5}
+export UUID=${UUID:-}
 export PATH_VLESS=${PATH_VLESS:-/$UUID/vless}
 export PATH_VMESS=${PATH_VMESS:-/$UUID/vmess}
 export WARP_SERVER=${WARP_SERVER:-engage.cloudflareclient.com}
